9e1f969aa5b579a46445a9fdb33e734052cda9f9
CHOISEOKRYEOL <zlshsktl@naver.com>
2021-10-07 12:05:33 +0900

    JS: Home contact button

--- main.js
+++ main.js
@@ -20,7 +20,21 @@ navbarMenu.addEventListener('click', (event) => {
     if(link == null) {
         return;
     }
-    console.log(event.target.dataset.link); 
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    // const scrollTo = document.querySelector(link);
+    // scrollTo.scrollIntoView({behavior: 'smooth'});
+
+    scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', (event) => {
+    // const link = event.target.dataset.link;
+    // const scrollTo = document.querySelector(link); or
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior: 'smooth'});
+}
